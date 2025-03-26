24f3341736fe4ddb398d3551562f90c90bc501b3
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-03-25 23:10:23 -0300

    feat: try it out section added

--- resources/js/src/pages/Home/index.jsx
+++ resources/js/src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { Link } from '@inertiajs/react'
 import github from '../../assets/github.svg'
 import docs from '../../assets/docs.svg'
 import Card from '../../components/Card'
+import codeExample from '../../data/codeExample'
 
 const Home = () => {
   return (
@@ -27,11 +28,21 @@ const Home = () => {
           </Link>
         </div>
       </main>
-      <section className='container mx-auto px-4 fornt-montserrat text-center card-section'>
+      <section className='container mx-auto px-4 font-montserrat text-center card-section'>
         <div className='flex flex-wrap justify-center'>
           <Card title='Real-time Data' content='Get pseudo-real data for your e-commerce projects without running server-side code.' />
           <Card title='Easy Integration' content='Simple to use with any frontend framework or vanilla JavaScript.' />
           <Card title='Customizable' content='Tailor the API responses to fit your specific project needs.' />
+        </div>
+      </section>
+      <section className='container mx-auto px-4 py-4 md:py-20 font-montserrat text-center code-example-section'>
+        <div className='md:px-6'>
+          <div className='card border border-gray-300 p-5 rounded-md mx-2 my-2 text-left font-montserrat hover:shadow-cardShadow'>
+            <h2 className='text-2xl font-bold mb-4'>Try it out</h2>
+            <pre className='px-4 py-2 bg-gray-100 rounded-md overflow-auto'>
+              <code>{codeExample}</code>
+            </pre>
+          </div>
         </div>
       </section>
     </>
